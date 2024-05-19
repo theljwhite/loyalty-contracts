e2221904ccbfccd3e42fb706cd67170fd5e202df
LJ White <theljwhite@gmail.com>
2024-05-19 16:13:47 -0400

    continued tests for erc20 escrow

--- test/0.03/TestERC20EscrowUnits.ts
+++ test/0.03/TestERC20EscrowUnits.ts
@@ -6,15 +6,18 @@ import { moveTime } from "../../utils/moveTime";
 import { deployProgramAndSetUpUntilDepositPeriod } from "../../utils/deployLoyaltyUtils";
 import {
   ERC20RewardCondition,
+  EscrowState,
+  LoyaltyState,
   RewardType,
 } from "../../constants/contractEnums";
-import { getERC20UserProgress } from "../../utils/userProgressTestUtils";
+import keccak256 from "keccak256";
 import {
   calculateRootHash,
   getAppendProof,
   getUpdateProof,
 } from "../../utils/merkleUtils";
 import { depositKeyBytes32 } from "../../constants/basicLoyaltyConstructorArgs";
+import { time } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 
 let accounts: SignerWithAddress[] = [];
 let creatorOne: SignerWithAddress;
@@ -24,7 +27,6 @@ let relayer: SignerWithAddress;
 
 let userOne: SignerWithAddress;
 let userTwo: SignerWithAddress;
-let userThree: SignerWithAddress;
 
 let programOne: any;
 let escrowOne: any;
@@ -48,7 +50,6 @@ describe("LoyaltyProgram", async () => {
     relayer = accounts[5];
     userOne = accounts[10];
     userTwo = accounts[11];
-    userThree = accounts[12];
 
     //deploy test ERC20 token to be used for ERC20 escrow rewards
     //transfer amount from minter to creatorOne to deposit as rewards
@@ -253,7 +254,227 @@ describe("LoyaltyProgram", async () => {
 
     expect(formattedStatePayoutAmounts).deep.equal(correctPayoutShape);
   });
-  it("ensures proper user balance behavior when a rewards contract has non-stanard decimals", async () => {
-    //...TODO
+  it("ensures proper user balance behavior still happens when a rewards contract has standard decimals", async () => {
+    //set program to active
+    await programOne.connect(creatorOne).setLoyaltyProgramActive();
+
+    const programOneState = await programOne.state();
+    const escrowOneState = await escrowOne.escrowState();
+
+    expect(programOneState).equal(LoyaltyState.Active, "Incorrect");
+    expect(escrowOneState).equal(EscrowState.InIssuance, "Incorrect");
+
+    //complete 2 objectives as userOne for programOne
+    //use merkle root flow for now altho may be temporary
+    const objectiveIndexZero = 0;
+    const timestamp = await time.latest();
+    const message = `${objectiveIndexZero}${timestamp}`;
+    const messageHash = keccak256(message);
+    const signature = await relayer.signMessage(
+      hre.ethers.utils.arrayify(messageHash)
+    );
+
+    const appendProof = getAppendProof(treeAddresses);
+    await programOne
+      .connect(relayer)
+      .completeUserAuthorityObjective(
+        0,
+        userOne.address,
+        appendProof,
+        messageHash,
+        signature
+      );
+
+    treeAddresses.push(userOne.address);
+    calculateRootHash(treeAddresses);
+
+    const contractMerkleIndex = await programOne.getMerkleIndex(
+      userOne.address
+    );
+    const userOneMerkleIndex = contractMerkleIndex.toNumber();
+    const userOneProof = getUpdateProof(treeAddresses, userOneMerkleIndex);
+    const objectiveIndexOne = 1;
+
+    await programOne
+      .connect(relayer)
+      .completeUserAuthorityObjective(
+        objectiveIndexOne,
+        userOne.address,
+        userOneProof,
+        hre.ethers.constants.HashZero,
+        "0x"
+      );
+
+    //0.0002 tokens are rewarded per objective in programOne
+    //ensure escrow balance decreased from rewarding both objectives.
+    //original deposit was 0.6 and 2 objectives were completed for 0.0002 tokens each
+    const escrowBalance = await escrowOne.lookupEscrowBalance();
+    const formattedEscrowBal = hre.ethers.utils.formatUnits(
+      escrowBalance,
+      "ether"
+    );
+    expect(formattedEscrowBal).equal("0.5996", "Incorrect escrow balance");
+
+    //ensure user balance increased from rewarding both objectives.
+    //user balance should now be 0.0004 tokens, 0.0002 for each objective completed
+    const userBalance = await escrowOne
+      .connect(creatorOne)
+      .lookupUserBalance(userOne.address);
+    const formattedUserBal = hre.ethers.utils.formatUnits(userBalance, "ether");
+
+    expect(formattedUserBal).equal("0.0004", "Incorrect user balance");
+
+    //withdraw tokens as userOne, ensure handling is still correct
+    const initialUserWalletBalance = await testToken.balanceOf(userOne.address);
+    expect(initialUserWalletBalance.toNumber()).equal(0, "Incorrect");
+
+    //first, withdraw with userWithdraw() and withdraw only 0.0001.
+    //ensure user balance in contract decreased and wallet balanace increased
+    const withdrawAmountOne = hre.ethers.utils.parseUnits("0.0001", "ether");
+    await escrowOne.connect(userOne).userWithdraw(withdrawAmountOne);
+
+    const userContractBalOne = await escrowOne
+      .connect(creatorOne)
+      .lookupUserBalance(userOne.address);
+    const formattedUserContractBal = hre.ethers.utils.formatUnits(
+      userContractBalOne,
+      "ether"
+    );
+
+    expect(formattedUserContractBal).equal(
+      "0.0003",
+      "Incorrect contract bal after 1st withdraw"
+    );
+
+    //withdraw remaining balance as userOne with userWithdrawAll()
+    await escrowOne.connect(userOne).userWithdrawAll();
+
+    const finalUserOneContractBal = await escrowOne
+      .connect(creatorOne)
+      .lookupUserBalance(userOne.address);
+
+    expect(finalUserOneContractBal.toNumber()).equal(
+      0,
+      "Should have no remaining bal"
+    );
+
+    //ensure userOne's wallet balance is correct
+    const finalUserWalletBal = await testToken.balanceOf(userOne.address);
+    const formattedFinalUserWalletBal = hre.ethers.utils.formatUnits(
+      finalUserWalletBal,
+      "ether"
+    );
+
+    expect(formattedFinalUserWalletBal).equal(
+      "0.0004",
+      "Incorrect user one wallet bal"
+    );
+  });
+  it("ensures proper user balance behavior still happens when a rewards contract does not have standard decimals", async () => {
+    await programTwo.connect(creatorTwo).setLoyaltyProgramActive();
+
+    const programTwoState = await programTwo.state();
+    const escrowTwoState = await escrowTwo.escrowState();
+
+    expect(programTwoState).equal(LoyaltyState.Active, "Incorrect");
+    expect(escrowTwoState).equal(EscrowState.InIssuance, "Incorrect");
+
+    //complete 2 objectives as userTwo for programTwo
+    //use merkle root flow for now altho may be temporary
+
+    const objectiveIndexZero = 0;
+    const timestamp = await time.latest();
+    const message = `${objectiveIndexZero}${timestamp}`;
+    const messageHash = keccak256(message);
+    const signature = await relayer.signMessage(
+      hre.ethers.utils.arrayify(messageHash)
+    );
+
+    const appendProof = getAppendProof(treeAddresses);
+    await programTwo
+      .connect(relayer)
+      .completeUserAuthorityObjective(
+        0,
+        userTwo.address,
+        appendProof,
+        messageHash,
+        signature
+      );
+
+    treeAddresses.push(userTwo.address);
+    calculateRootHash(treeAddresses);
+
+    const contractMerkleIndex = await programTwo.getMerkleIndex(
+      userTwo.address
+    );
+    const userTwoMerkleIndex = contractMerkleIndex.toNumber();
+    const userTwoProof = getUpdateProof(treeAddresses, userTwoMerkleIndex);
+    const objectiveIndexOne = 1;
+
+    await programTwo
+      .connect(relayer)
+      .completeUserAuthorityObjective(
+        objectiveIndexOne,
+        userTwo.address,
+        userTwoProof,
+        hre.ethers.constants.HashZero,
+        "0x"
+      );
+
+    //1 token (represented as 1000000) rewarded per objective in programOne
+    //ensure escrow balance decreased from rewarding both objectives.
+    //original deposit was 100 (100000000) and 2 objectives were completed for 1 tokens each
+    const escrowBalance = await escrowTwo.lookupEscrowBalance();
+    const formattedEscrowBal = hre.ethers.utils.formatUnits(escrowBalance, 6);
+
+    expect(formattedEscrowBal).equal("98000000.0", "Incorrect");
+
+    //withdraw tokens as userTwo, ensure handling is still correct
+    const initialUserWalletBalance = await testTokenTwo.balanceOf(
+      userTwo.address
+    );
+    expect(initialUserWalletBalance.toNumber()).equal(0, "Incorrect");
+
+    //first, withdraw with userWithdraw() and withdraw only 1 token
+    //ensure user balance is contract decreased and wallet balance increased
+    const withdrawAmountOne = hre.ethers.utils.parseUnits("1000000", 6);
+    await escrowTwo.connect(userTwo).userWithdraw(withdrawAmountOne);
+
+    const userContractBalOne = await escrowTwo
+      .connect(creatorTwo)
+      .lookupUserBalance(userTwo.address);
+    const formattedUserContractBal = hre.ethers.utils.formatUnits(
+      userContractBalOne,
+      6
+    );
+
+    expect(formattedUserContractBal).equal(
+      "1000000.0",
+      "Incorrect contract bal after 1st withdraw "
+    );
+
+    //withdraw remaining balance with userWtihdrawAll();
+    await escrowTwo.connect(userTwo).userWithdrawAll();
+
+    const finalUserTwoContractBal = await escrowTwo
+      .connect(creatorTwo)
+      .lookupUserBalance(userTwo.address);
+
+    expect(finalUserTwoContractBal.toNumber()).equal(
+      0,
+      "Should have no remaining bal"
+    );
+
+    //ensure userTwo's wallet balance is correct
+    const finalUserWalletBal = await testTokenTwo.balanceOf(userTwo.address);
+    const formattedFinalUserWalletBal = hre.ethers.utils.formatUnits(
+      finalUserWalletBal,
+      6
+    );
+
+    expect(formattedFinalUserWalletBal).equal(
+      "2000000.0",
+      "Incorrect user two wallet bal"
+    );
   });
 });
